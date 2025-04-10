78b864677a3229cc3fc4e55ab4097f76ba56a3f7
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-04-10 16:13:05 +0530

    change in faq

--- js/app.js
+++ js/app.js
@@ -1,30 +1,59 @@
 // faq
 function toggleAccordion(button) {
-    const allAccordions = document.querySelectorAll(".accordin-data");
-    const allIcons = document.querySelectorAll(
-        ".accordin-button span:last-child"
-    );
-
     const paragraph = button.nextElementSibling;
-    const icon = button.querySelector("span:last-child");
+    const icon = button.querySelector(".rotate-icon");
 
-    allAccordions.forEach((otherParagraph) => {
-        if (otherParagraph !== paragraph) {
-            otherParagraph.style.maxHeight = null;
+    const allParagraphs = document.querySelectorAll(".accordin-data");
+    const allIcons = document.querySelectorAll(".rotate-icon");
+
+
+    allParagraphs.forEach(p => {
+        if (p !== paragraph) {
+            p.style.maxHeight = null;
         }
     });
 
-    allIcons.forEach((otherIcon) => {
-        if (otherIcon !== icon) {
-            otherIcon.classList.add("after:rotate-90");
+    allIcons.forEach(i => {
+        if (i !== icon) {
+            i.classList.remove("rotate-180");
         }
     });
 
     if (paragraph.style.maxHeight) {
         paragraph.style.maxHeight = null;
-        icon.classList.add("after:rotate-90");
+        icon.classList.remove("rotate-180");
     } else {
         paragraph.style.maxHeight = paragraph.scrollHeight + "px";
-        icon.classList.remove("after:rotate-90");
+        icon.classList.add("rotate-180");
     }
 }
+
+
+$('.img-slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    infinite: true,
+    // autoplay: true,
+    autoplaySpeed: 500,
+    slidesToShow: 4,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
